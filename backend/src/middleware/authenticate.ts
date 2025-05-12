a737703bca71e7ee990fbc9e5f9ca857c9e3b70e
stm <kingsj1418@gmail.com>
2025-05-12 17:03:43 +0900

    토큰 만료시 로그아웃 설정

--- backend/src/middleware/authenticate.ts
+++ backend/src/middleware/authenticate.ts
@@ -8,20 +8,23 @@ export interface AuthRequest extends Request {
 }
 
 export const authenticate = async (req: AuthRequest, res: Response, next: NextFunction): Promise<void> => {
-  const token = req.cookies.token;       // cookie-parser 필요
+  const token = req.cookies['token'];       // cookie-parser 필요
   if (!token) {
-    res.status(401).send('토큰이 없습니다.');
+    res.status(401).json({ message: '토큰이 없습니다.' });
     return;
   } 
   try {
+     // 1) 토큰 검증
     const payload = jwt.verify(token, process.env.JWT_SECRET!) as any;
     req.user = { id: payload.id, role: payload.role };
     next();
   } catch(err:any) {
+    // 2) 토큰 만료 처리
     if (err instanceof TokenExpiredError) {
-      // 1) 쿠키 삭제
-      res.clearCookie('token');
-      // 2) DB isLoggedIn 초기화
+      // 2-1) 쿠키 삭제
+      res.clearCookie('token', { path: '/' });
+      
+      // 2-2) DB isLoggedIn 초기화
       const decoded = jwt.decode(token) as { id?: string };
       if (decoded?.id) {
         await User.findByIdAndUpdate(decoded.id, { isLoggedIn: false });
