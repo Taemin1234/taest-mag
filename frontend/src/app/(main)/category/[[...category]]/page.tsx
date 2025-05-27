bec46fc23c391627bfd52ba74bdb2647bf5fefdb
King sejong <cestmoi7531@gmail.com>
2025-05-27 22:13:20 +0900

    catch-all segment 설정정

--- frontend/src/app/(main)/category/[[...category]]/page.tsx
+++ frontend/src/app/(main)/category/[[...category]]/page.tsx
@@ -7,18 +7,21 @@ interface CategoryPageProps {
 }
 
 export default async function CategoryPage({ params }: CategoryPageProps) {
-  const { category } = params
+  const { category } = await params
+
+  console.log(category)
 
   // 서버 컴포넌트이므로 fetch 또는 DB 호출도 직접!
-//   const posts = await getPostsByCategory(category)
+  //   const posts = await getPostsByCategory(category)
 
   return (
     <main>
-      <h1>‘{category}’ 카테고리 게시물</h1>
-      {posts.length
+      <h1>‘{category[0]} / {category[1]}’ 카테고리 게시물</h1>
+      {/* {posts.length
         ? <PostList posts={posts} />
         : <p>게시물이 없습니다.</p>
-      }
+      } */}
+
     </main>
   )
 }
--- frontend/src/components/PostList.tsx
+++ frontend/src/components/PostList.tsx
@@ -6,38 +6,40 @@ import React, { useState, useEffect } from 'react';
 import { Post } from "@/types"
 import { fetchPosts } from '@/lib/api'
 
-export default function PostList()  {
+export default function PostList() {
     const [posts, setPosts] = useState<Post[]>([]);
     const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
         setIsLoading(true)
         fetchPosts()
-        .then((data: Post[]) => {
-            setPosts(data)
-        })
-        .catch((err) => {
-            console.error('게시물 로딩 실패:', err)
-        })
-        .finally(() => {
-            setIsLoading(false)
-        })
+            .then((data: Post[]) => {
+                setPosts(data)
+            })
+            .catch((err) => {
+                console.error('게시물 로딩 실패:', err)
+            })
+            .finally(() => {
+                setIsLoading(false)
+            })
     }, []);
 
     return (
-        <div>
-           {posts.map((post) => (
-                <Link href={`/post/${post.id}`} key={post.slug}>
-                    <div className={styles.post_list}>
-                        <div className="flex justify-between">
-                            <p className="text-sm text-gray-500">{post.category}</p>
-                            <p className="text-sm text-gray-500">{new Date(post.createdAt).toLocaleDateString('ko-KR')}</p>
+        <ul className={styles.postlist_wrap}>
+            {posts.map((post) => (
+                <li key={post.slug}>
+                    <Link href={`/post/${post.id}`} >
+                        <div className={styles.post_list}>
+                            <div className="flex justify-between">
+                                <p className="text-sm text-gray-500">{post.category}</p>
+                                <p className="text-sm text-gray-500">{new Date(post.createdAt).toLocaleDateString('ko-KR')}</p>
+                            </div>
+                            <p className="mt-2 font-semibold">{post.title}</p>
+                            <p className="text-sm text-gray-600">{post.subtitle}</p>
                         </div>
-                        <p className="mt-2 font-semibold">{post.title}</p>
-                        <p className="text-sm text-gray-600">{post.subtitle}</p>
-                    </div>
-                </Link>
+                    </Link>
+                </li>
             ))}
-        </div>
+        </ul>
     )
 }
