e5aaa2de1dfbe8a5055ecfb9e6c7dda0fcee1be0
King sejong <cestmoi7531@gmail.com>
2025-10-20 22:27:36 +0900

    임시 테스트 파일

---
+++ backend/api/hello.ts
@@ -0,0 +1,6 @@
+// backend/api/hello.ts
+import type { VercelRequest, VercelResponse } from '@vercel/node';
+
+export default function handler(req: VercelRequest, res: VercelResponse) {
+    res.status(200).send('hello-ok');
+}
