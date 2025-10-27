b6caf5ef1d8ec42dc8b1375ca736c868f89f6ea0
King sejong <cestmoi7531@gmail.com>
2025-10-27 21:34:28 +0900

    rewrites 문제 확인

--- backend/api/index.ts
+++ backend/api/index.ts
@@ -1,5 +1,9 @@
 // backend/api/index.ts
-import serverless from 'serverless-http';
-import app from '../src/app';
+// import serverless from 'serverless-http';
+// import app from '../src/app';
 
-export default serverless(app);
+// export default serverless(app);
+
+export default function handler(req: any, res: any) {
+    res.status(200).json({ ok: true, path: req.url })
+}
