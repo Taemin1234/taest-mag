efc149bc71245eaf304b34d0a551925d2f52d41c
King sejong <cestmoi7531@gmail.com>
2025-10-28 19:28:13 +0900

    index.ts 원상복구

--- backend/api/index.ts
+++ backend/api/index.ts
@@ -1,9 +1,5 @@
 // backend/api/index.ts
-// import serverless from 'serverless-http';
-// import app from '../src/app';
+import serverless from 'serverless-http';
+import app from '../src/app';
 
-// export default serverless(app);
-
-export default function handler(req: any, res: any) {
-    res.status(200).json({ ok: true, path: req.url })
-}
+export default serverless(app);
